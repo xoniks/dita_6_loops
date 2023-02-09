f5b1795572f9d26771f27bdea175696fdffb3688
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-02-09 22:00:05 +0100

    print_not_all_added

--- loops.py
+++ loops.py
@@ -1,21 +1,31 @@
 
 from typing import types
 
-def fib_from_text(text_input:str)->list:
+def fib_from_text(text_input:str, print_not_all=True)->list:
     
     ''' This functions has one parameter it is a text
         and returns a list of fibonaci numbers with 
         last number not bigger than the length of text_input.'''
         
     num_list = [1]
+    
+        
 
     for i in range(len(text_input)-1):
-        if num_list[-1]<len(text_input):
+        
+        if print_not_all:
+            if num_list[-1]<len(text_input):
+                if not(text_input[i].isspace()):
+                    if len(num_list)==1:
+                        num_list.append(1)
+                    else:
+                        num_list.append(num_list[-1]+num_list[-2])
+        else:
             if not(text_input[i].isspace()):
-                if len(num_list)==1:
-                    num_list.append(1)
-                else:
-                    num_list.append(num_list[-1]+num_list[-2])    
+                    if len(num_list)==1:
+                        num_list.append(1)
+                    else:
+                        num_list.append(num_list[-1]+num_list[-2])
                     
     if num_list[-1]>len(text_input):
         num_list=num_list[:-1]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from loops import fib_from_text
 
 
-text_input = 'Lorem ipsum ipsum Lorem ipsum ipsum Lorem ipsum ipsum Lorem ipsum ipsum'
-result = fib_from_text(text_input)
+text_input = 'Lorem ipsum ipsum '
+result = fib_from_text(text_input,print_not_all=True)
 print(len(text_input))
 print(result)
